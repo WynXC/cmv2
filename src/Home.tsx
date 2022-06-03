98b7945236a771ffb9904235397eb6dfa85f8817
Wync <Tootie-@live.com.sg>
2022-06-03 17:00:49 +0800

    .

--- src/Home.tsx
+++ src/Home.tsx
@@ -509,7 +509,7 @@ const Home = (props: HomeProps) => {
                         fontWeight: 'bold',
                       }}
                     >
-                      {`698`}
+                      {`688`}
                     </Typography>
                   </Grid>
                   <Grid item xs={4}>
